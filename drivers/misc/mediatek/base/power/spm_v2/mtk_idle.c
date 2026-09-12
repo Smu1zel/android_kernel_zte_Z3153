ab69f3fb0f5673ea8763e9cfe90755cdc311bc9c
Smu1zel <lyndenl25@yahoo.com>
2026-09-12 07:13:00 -0400

    Z3153: Use bfd to support OVERLAY. Fix mtk-idle build issues.

--- drivers/misc/mediatek/base/power/spm_v2/mtk_idle.c
+++ drivers/misc/mediatek/base/power/spm_v2/mtk_idle.c
@@ -2998,7 +2998,6 @@ void __init mtk_cpuidle_framework_init(void)
 	mtk_idle_hotplug_cb_init();
 	mtk_idle_profile_init();
 }
-EXPORT_SYMBOL(mtk_cpuidle_framework_init);
 
 
 module_param(mtk_idle_chk_golden, bool, 0644);
