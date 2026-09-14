d99fab5d7f2637f490bebf9995b87ba796243c67
Smu1zel <lyndenl25@yahoo.com>
2026-09-14 08:55:49 -0400

    Z3153: atomic: Define atomic_cond_read_relaxed and atomic_cond_read_acquire
    - Needed by fs/erofs/internal.h:erofs_wait_on_workgroup_freezed()
    - Wraps smp_cond_load_relaxed() on the atomic counter

--- include/asm-generic/atomic-long.h
+++ include/asm-generic/atomic-long.h
@@ -243,4 +243,14 @@ static inline long atomic_long_add_unless(atomic_long_t *l, long a, long u)
 #define atomic_long_inc_not_zero(l) \
 	ATOMIC_LONG_PFX(_inc_not_zero)((ATOMIC_LONG_PFX(_t) *)(l))
 
+#ifndef atomic_long_cond_read_relaxed
+#define atomic_long_cond_read_relaxed(v, c) \
+	ATOMIC_LONG_PFX(_cond_read_relaxed)((ATOMIC_LONG_PFX(_t) *)(v), (c))
+#endif
+
+#ifndef atomic_long_cond_read_acquire
+#define atomic_long_cond_read_acquire(v, c) \
+	ATOMIC_LONG_PFX(_cond_read_acquire)((ATOMIC_LONG_PFX(_t) *)(v), (c))
+#endif
+
 #endif  /*  _ASM_GENERIC_ATOMIC_LONG_H  */
