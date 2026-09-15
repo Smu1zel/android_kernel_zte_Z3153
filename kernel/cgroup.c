e634c2f1f41c632d6e8586c6039b6e831b3a6767
Smu1zel <lyndenl25@yahoo.com>
2026-09-15 01:13:29 -0400

    Z3153: Add hack to handle Cgroups options kernel 4.9 can't do Taken from https://github.com/mt8163/android_kernel_amazon_karnak_4.9/commit/319b74646bca1c5bee5ef9b63a0dfc5ac543f704

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -1735,6 +1735,13 @@ static int parse_cgroupfs_options(char *data, struct cgroup_sb_opts *opts)
 			opts->flags |= CGRP_ROOT_NOPREFIX;
 			continue;
 		}
+		if (!strcmp(token, "cpuset_v2_mode")) {
+			/* Accepted as no-op. The option was introduced in 4.20
+			 * to make v1 cpuset preserve cpus_allowed across hotplug.
+			 * Android libprocessgroup passes this option unconditionally;
+			 * without accepting it, the required cpuset mount fails. */
+			continue;
+		}
 		if (!strcmp(token, "clone_children")) {
 			opts->cpuset_clone_children = true;
 			continue;
@@ -2127,7 +2134,17 @@ static struct dentry *cgroup_mount(struct file_system_type *fs_type,
 
 	if (is_v2) {
 		if (data) {
-			pr_err("cgroup2: unknown option \"%s\"\n", (char *)data);
+			char *opts = data;
+			char *token;
+
+			while ((token = strsep(&opts, ",")) != NULL) {
+			if (!*token ||
+			    !strcmp(token, "memory_recursiveprot") ||
+			    !strcmp(token, "nsdelegate") ||
+			    !strcmp(token, "memory_localevents"))
+				continue;
+
+			pr_err("cgroup2: unknown option \"%s\"\n", token);
 			put_cgroup_ns(ns);
 			return ERR_PTR(-EINVAL);
 		}
